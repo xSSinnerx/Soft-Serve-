e0c9db22c600bb429437a100878fc1f4ca588c9b
xSSinnerx <61522712+xSSinnerx@users.noreply.github.com>
2021-01-27 07:44:01 +0200

    Add files via upload

---
+++ Homework1.1.js
@@ -0,0 +1,21 @@
+let x;
+x = "Nestor"
+console.log (x);
+let y;
+y = "Khomytskyy"
+console.log (y);
+let c;
+c = "ed.group"
+console.log (c);
+let v;
+v = "29.06.1988"
+console.log (v)
+let married;
+married = true;
+console.log (married);
+let b;
+b = null;
+console.log (b)
+let n;
+n = undefined;
+console.log (n)
